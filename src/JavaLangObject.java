f3eee2bd5323bebd3ad74fee770fd359e4fea9bd
RakibulHaque <hrakibul@sun21.co.jp>
2019-10-09 17:35:19 +0900

    commit

---
+++ src/JavaLangObject.java
@@ -0,0 +1,23 @@
+
+public class JavaLangObject {
+	 public static void main(String arr[]) 
+	    { 
+	        Object y; 
+	  
+	        y = 'A'; 
+	        System.out.println(y.getClass().getName()); 
+	  
+	        y = 1; 
+	        System.out.println(y.getClass().getName()); 
+	  
+	        y = "Hi"; 
+	        System.out.println(y.getClass().getName()); 
+	  
+	        y = 1.222; 
+	        System.out.println(y.getClass().getName()); 
+	  
+	        y = false; 
+	        System.out.println(y.getClass().getName()); 
+	    } 
+
+}
